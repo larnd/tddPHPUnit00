3c53a9ab4b704ae0877055b5466915f84dbd364f
Andreas Larsson <datastuff@gmail.com>
2021-03-05 14:56:18 +0100

    Refactoring with phpUnit dataprovider

--- test/NumericArrayTest.php
+++ test/NumericArrayTest.php
@@ -6,34 +6,27 @@ use PHPUnit\Framework\TestCase;
 
 final class NumericArrayTest extends TestCase
 {
-    public function test_getAverage_should_return_zero_on_empty_array(): void
-    {
-        $input = array();       
-        $expected =0;
+    /**
+     * @dataProvider averageProvider
+     */    
 
-        $this->assert_getAverage($input, $expected);
+    public function test_getAverage($input, $expected) : void {
+        
+        $sut = new NumericArray($input);
+        
+        $actual = $sut->getAverage();
+        
+        $this->assertEquals($expected, $actual);
     }
 
-    public function test_average_should_return_the_first_item_for_single_item_arrays() : void {
-        $input = array(1);
-        $expected =1;
-      
-        $this->assert_getAverage($input, $expected);
-    }
-
-    public function test_average_should_return_the_average_of_two_items() : void {
-        $input = array(1,2);
-        $expected =1.5;
-      
-        $this->assert_getAverage($input, $expected);
-    }
-
-    public function test_average_should_return_the_average_of_three_items() : void {
-        $input = array(1,2,3);
-        $expected =(1+2+3)/3.0;
-      
-        $this->assert_getAverage($input, $expected);
-    }
+    public function averageProvider(): array{
+        return[
+            'should_return_zero_on_empty_array' =>[array(), 0],
+            'should_return_the_first_item_for_single_item_arrays' =>[array(1),1],
+            'should_return_the_average_of_two_items' =>[array(1,2), 1.5],
+            'should_return_the_average_of_three_items' =>[array(1,2,3),(1+2+3)/ 3.0]
+        ];
+            }
 
     //Helper function for refactoring
     private function assert_getAverage(array $input, float $expected)
@@ -43,4 +36,5 @@ final class NumericArrayTest extends TestCase
         $actual = $sut->getAverage();          
         $this->assertEquals($expected, $actual);
     }
+
 }
